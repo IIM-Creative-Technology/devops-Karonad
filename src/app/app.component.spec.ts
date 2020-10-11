46ea7c3f09c1e0ecabbaf224fa46743164534e2d
Quentin Flé <44965475+Karonad@users.noreply.github.com>
2020-10-11 21:57:12 +0200

    Add httpClient

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        HttpClientModule
+      ]
     }).compileComponents();
   }));
 
